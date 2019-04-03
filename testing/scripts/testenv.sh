ef8d7532e357cfe5b01f50588b72488010d21d7e
Gerhard Wesp <kisstech.ch@gmail.com>
2019-04-03 09:55:58 +0200

    MOD: Use cmake build in run-all-tests

--- testing/scripts/testenv.sh
+++ testing/scripts/testenv.sh
@@ -1,7 +1,11 @@
-if [ true = "$debug" ] ; then
-  BIN_DIR=../bin/dbg
-else 
-  BIN_DIR=../bin/opt
+if [ -d ../build ] ; then
+  BIN_DIR=../build
+else
+  if [ true = "$debug" ] ; then
+    BIN_DIR=../bin/dbg
+  else 
+    BIN_DIR=../bin/opt
+  fi
 fi
 
 GOLDEN_DIR=data/golden-output

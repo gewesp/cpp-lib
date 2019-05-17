ab0757e9252066a3a742ddacd054115598088813
Gerhard Wesp <kisstech.ch@gmail.com>
2019-05-17 13:21:30 +0200

    MOD: Generalize and simplify memory_consumption()

--- include/cpp-lib/memory.h
+++ include/cpp-lib/memory.h
@@ -23,26 +23,68 @@
 #pragma once
 
 #include "cpp-lib/assert.h"
+#include "cpp-lib/type_traits.h"
 
 #include <string>
 
 #include <type_traits>
 
+namespace cpl::util {
+
+/// @return Estimate of memory used by the given object
+template <typename T> long memory_consumption(const T&);
+
+} // namespace cpl::util
+
+
+////////////////////////////////////////////////////////////////////////
+// Implementation details only below this line
+////////////////////////////////////////////////////////////////////////
+
 namespace cpl::detail_ {
 
 long memory_consumption_overloaded(const std::string&);
 
+/// @return Estimate of memory consumption for containers.  Fast iff
+/// value is a POD.
+template <typename C>
+long memory_consumption_container(const C& c) {
+  using ::cpl::util::memory_consumption;
+
+  if constexpr(std::is_trivially_copyable<typename C::value_type>::value) {
+    return sizeof(C::value_type) * c.size();
+  } else {
+    long ret = 0;
+    for (const auto& el : c) {
+      ret += memory_consumption(el);
+    }
+    return ret;
+  }
+}
+
+template <typename P>
+long memory_consumption_pair(const P& p) {
+  using ::cpl::util::memory_consumption;
+  return memory_consumption(p.first) + memory_consumption(p.second);
+}
+
 } // namespace cpl::detail_
 
-namespace cpl::util {
 
-/// @return Estimate of memory used by the given object
-template <typename T>
-long memory_consumption(const T& x) {
+
+////////////////////////////////////////////////////////////////////////
+// Template definitions
+////////////////////////////////////////////////////////////////////////
+
+template <typename T> long ::cpl::util::memory_consumption(const T& x) {
   if constexpr(std::is_trivially_copyable<T>::value) {
     return sizeof(T);
   } else if constexpr(std::is_same<typename std::remove_cv<T>::type, std::string>::value) {
     return ::cpl::detail_::memory_consumption_overloaded(x);
+  } else if constexpr(::cpl::util::is_container<T>::value) {
+    return ::cpl::detail_::memory_consumption_container(x);
+  } else if constexpr(::cpl::util::is_pair<T>::value) {
+    return ::cpl::detail_::memory_consumption_pair(x);
   } else {
     // Doesn't work ... Did the committee not consider static_assert
     // in conjunction with constexpr if()?
@@ -50,26 +92,3 @@ long memory_consumption(const T& x) {
     always_assert(not "memory_consumption() called for unsupported type");
   }
 }
-
-/// @return Estimate of memory consumption for containers (slow)
-template <typename C>
-long memory_consumption_container(const C& c) {
-  long ret = 0;
-  for (const auto& el : c) {
-    ret += memory_consumption(el);
-  }
-  return ret;
-}
-
-/// @return Estimate of memory consumption for a std::map (slow)
-template <typename M>
-long memory_consumption_map(const M& m) {
-  long ret = 0;
-  for (const auto& el : m) {
-    ret += memory_consumption(el.first )
-        +  memory_consumption(el.second);
-  }
-  return ret;
-}
-
-} // namespace cpl::util

f67587e18a1ca4647339938ac5bead909e33c514
Gerhard Wesp <kisstech.ch@gmail.com>
2019-10-13 20:49:48 +0000

    FIX: Locales? ISSUE: #12

--- scripts/install-requirements-ubuntu.sh
+++ scripts/install-requirements-ubuntu.sh
@@ -11,17 +11,18 @@
 # Exit if any subprogram exits with an error
 set -e
 
-# Language-pack-en required to install en_US.utf-8 locale
+# Locales required to install en_US.utf-8 locale
 apt-get update --yes
 apt-get install --yes cmake       \
-  language-pack-en                \
+  locales                         \
   libeigen3-dev                   \
   libpng-dev libpng++-dev         \
   libboost-dev
 
 # https://community.appdynamics.com/t5/Dynamic-Languages-Node-JS-Python/locale-facet-S-create-c-locale-name-not-valid/td-p/33693
 # Note: Argument of locale-gen undocumented in man page...
-locale-gen locale-gen en_US.UTF-8
+locale-gen en_US.UTF-8
+# update-locale LANG=en_US.UTF-8
 
 if [[ "" == $GITLAB_CI ]] ; then
   apt-get install --yes             \

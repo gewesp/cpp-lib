0f9ed195419e2769030da831b7fe1235606dcd58
Gerhard Wesp <kisstech.ch@gmail.com>
2019-10-13 10:05:24 +0000

    MOD: Use gcc on CI ISSUE: #6

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,14 @@
 # Exit if any subprogram exits with an error
 set -e
 
+if [[ "" == $GITLAB_CI ]] ; then
+  # Local compiler
+  cmake_compiler="CMAKE_CXX_COMPILER=clang++-7"
+else
+  # Gitlab CI: Use whatever is available
+  cmake_compiler=""
+fi
+
 if [ "" = "$1" ] ; then
   build_type=RelWithDebInfo
 else
@@ -13,5 +21,5 @@ rm -rf build CMakeCache.txt CMakeFiles/
 mkdir build
 cd build
 echo "Configuring for $build_type ..."
-cmake -D CMAKE_CXX_COMPILER=clang++-7 -D CMAKE_BUILD_TYPE=$build_type ..
+cmake -D $cmake_compiler -D CMAKE_BUILD_TYPE=$build_type ..
 make -j6

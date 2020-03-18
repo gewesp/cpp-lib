93f73066ddc873600db2e9fbbf1fa0bc49ab462c
Gerhard Wesp <kisstech.ch@gmail.com>
2020-03-18 09:16:07 +0000

    MOD: Gitlab move

--- testing/scripts/run-all-tests.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-. scripts/testenv.sh
-
-./scripts/run-fast-tests.sh
-./scripts/run-annoying-tests.sh
-
-$BIN_DIR/spatial-index-test                 > $GOLDEN_DIR/spatial-index.txt
-$BIN_DIR/dispatch-test                      > $GOLDEN_DIR/dispatch.txt
